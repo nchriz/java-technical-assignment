f12f0a30f74427653d3da2f77d4c86ca35184788
nchriz <chma00@gmail.com>
2020-10-11 21:17:46 +0100

    Small cleanup, comment why use of Map

--- src/main/java/kata/supermarket/Basket.java
+++ src/main/java/kata/supermarket/Basket.java
@@ -30,10 +30,13 @@ public class Basket {
         TotalCalculator() {
             this.items = items();
             numberOfItems = new HashMap<>();
-            sameSetItemsInBasket();
+            itemInBasket();
         }
 
-        private void sameSetItemsInBasket() {
+        /**
+         * To process the basket faster for discount, group items that are the same together.
+         */
+        private void itemInBasket() {
             for (Item item : this.items) {
                 numberOfItems.compute(item, (k,v) -> {
                     if (v == null)
@@ -56,9 +59,11 @@ public class Basket {
          *  It is not likely to be the best place to do those calculations.
          *  Think about how Basket could interact with something
          *  which provides that functionality.
+         *
+         *  
          */
         private BigDecimal discounts() {
-            BigDecimal discount = new BigDecimal(0);
+            BigDecimal discount = BigDecimal.ZERO;
             for (Map.Entry<Item, Integer> entry : numberOfItems.entrySet()) {
                 if (entry.getValue() % 3 == 0) {
                     discount = discount.add(buyItemByUnitForOneLess(entry.getKey(), entry.getValue(), 3));
@@ -71,9 +76,8 @@ public class Basket {
 
         private BigDecimal buyItemByUnitForOneLess(Item item, Integer numberOfItems, Integer itemValue) {
             BigDecimal currentDiscount = item.price();
-            currentDiscount = currentDiscount.multiply(
+            return currentDiscount.multiply(
                     new BigDecimal(numberOfItems / itemValue).setScale(0, RoundingMode.DOWN));
-            return currentDiscount;
         }
 
         private BigDecimal calculate() {
